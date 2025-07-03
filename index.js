e16e6c7eb0a0faaac01eb85d3f02dfb201719ca8
pholunar <charmingbelle2010@gmail.com>
2025-07-03 13:49:21 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/Home.html');
+});
+
+app.get('/Home', (req, res) => {
+  res.sendFile(__dirname + '/Home.html');
+});
+
+app.get('/About', (req, res) => {
+  res.sendFile(__dirname + '/About.html');
+});
+
+app.get('/Contact', (req, res) => {
+  res.sendFile(__dirname + '/Contact.html');
+})
+
+app.get('/Review',  (req, res) => {
+  res.sendFile(__dirname + '/Review.html');
+})
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${3000}`)
+})
+;
